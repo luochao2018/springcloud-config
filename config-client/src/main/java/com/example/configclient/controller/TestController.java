f2959071f310f37522ef7462574e7475134d16c1
luochao <1152597708@qq.com>
2020-09-16 14:47:37 +0800

    更改了一些bug

--- config-client/src/main/java/com/example/configclient/controller/TestController.java
+++ config-client/src/main/java/com/example/configclient/controller/TestController.java
@@ -1,10 +1,12 @@
 package com.example.configclient.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope//用于解决不刷新问题
 public class TestController {
     @Value("${foo}")
     String foo;
